df2badc244d96c5ded1653878ec8885e2b650a21
luizfernandodejesusjunior7esco <luizfernandodejesusjunior7esco@gmail.com>
2025-03-28 11:24:21 +0000

    criação da função para atualizar o card

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,48 @@ import iconStar from "./images/icon-star.svg"
 
 export function App(){
   const [notaAvaliacao, setNotaAvaliacao] = useState(0)
+  const [submited, setSubmited] = useState(false)
 
   function handleAlterarNotaAvaliacao(nota) {
     setNotaAvaliacao(nota)
+  }
 
-    console.log(nota)
+  function handleSubmit() {
+    if (notaAvaliacao !== 0) {
+      setSubmited(true)
+      return
+    }
+
+    alert("Please, chose a rate button!")
   }
 
   return (
-    <div className="bg-gradient-dark mx-6 p-6 rounded-2xl text-white font-overpass">
-      <div className="bg-dark-blue p-4 w-fit rounded-full mb-4">
-        <img src={iconStar} alt="icone da estrela" />
+    <div>
+      {submited === false ? (
+        <div className="bg-gradient-dark mx-6 p-6 rounded-2xl text-white font-overpass">
+          <div className="bg-dark-blue p-4 w-fit rounded-full mb-4">
+            <img src={iconStar} alt="icone da estrela" />
+          </div>
+    
+          <h1 className="text-2xl font-bold mb-2.5">How did we do?</h1>
+    
+          <p className="text-light-gray text-sm leading-1 mb-6">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
+    
+          <div className="flex justify-between mb-6">
+            <input type="button" value="1" className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm leading-2 text-medium-gray focus:bg-medium-gray focus:text-white" onClick={() => handleAlterarNotaAvaliacao(1)}/>
+            <input type="button" value="2" className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm leading-2 text-medium-gray focus:bg-medium-gray focus:text-white" onClick={() => handleAlterarNotaAvaliacao(2)}/>
+            <input type="button" value="3" className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm leading-2 text-medium-gray focus:bg-medium-gray focus:text-white" onClick={() => handleAlterarNotaAvaliacao(3)}/>
+            <input type="button" value="4" className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm leading-2 text-medium-gray focus:bg-medium-gray focus:text-white" onClick={() => handleAlterarNotaAvaliacao(4)}/>
+            <input type="button" value="5" className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm leading-2 text-medium-gray focus:bg-medium-gray focus:text-white" onClick={() => handleAlterarNotaAvaliacao(5)}/>
+          </div>
+    
+          <button className="bg-orange w-full py-3 uppercase text-sm rounded-3xl tracking-1 font-bold" onClick={handleSubmit}>Submit</button>
       </div>
-
-      <h1 className="text-2xl font-bold mb-2.5">How did we do?</h1>
-
-      <p className="text-light-gray text-sm leading-1 mb-6">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
-
-      <div className="flex justify-between mb-6">
-        <input type="button" value="1" className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm leading-2 text-medium-gray" onClick={() => handleAlterarNotaAvaliacao(1)}/>
-        <input type="button" value="2" className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm leading-2 text-medium-gray" onClick={() => handleAlterarNotaAvaliacao(2)}/>
-        <input type="button" value="3" className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm leading-2 text-medium-gray" onClick={() => handleAlterarNotaAvaliacao(3)}/>
-        <input type="button" value="4" className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm leading-2 text-medium-gray" onClick={() => handleAlterarNotaAvaliacao(4)}/>
-        <input type="button" value="5" className="bg-dark-blue w-10.5 h-10.5 rounded-full text-sm leading-2 text-medium-gray" onClick={() => handleAlterarNotaAvaliacao(5)}/>
-      </div>
-
-      <button className="bg-orange w-full py-3 uppercase text-sm rounded-3xl tracking-1 font-bold">Submit</button>
+      ) : (
+        <div className="bg-gradient-dark mx-6 p-6 rounded-2xl text-white font-overpass">
+          <p>paragrafo</p>
+        </div>
+      )}
     </div>
   )
 }
