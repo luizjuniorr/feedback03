bea5f5d21410c9232accd8e6b29e69cdf616805c
luizfernandodejesusjunior7esco <luizfernandodejesusjunior7esco@gmail.com>
2025-03-31 18:56:57 +0000

    Finalização do segundo card

--- src/App.jsx
+++ src/App.jsx
@@ -42,10 +42,14 @@ export function App(){
           <button className="bg-orange w-full py-3 uppercase text-sm rounded-3xl tracking-1 font-bold" onClick={handleSubmit}>Submit</button>
       </div>
       ) : (
-        <div className="bg-gradient-dark mx-6 p-6 rounded-2xl text-white font-overpass">
-          <img src={illustrationThankYou} alt="" />
+        <div className="bg-gradient-dark mx-6 p-6 rounded-2xl text-white font-overpass text-center">
+          <img className="mx-auto mb-6" src={illustrationThankYou} alt="" />
           
-          <p></p>
+          <p className="mb-6 text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl">You selected {notaAvaliacao} out of 5!</p>
+
+          <h1 className="text-2xl font-bold mb-2.5">Thank You!</h1>
+    
+          <p className="text-light-gray text-sm leading-1">We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
         </div>
       )}
     </div>
