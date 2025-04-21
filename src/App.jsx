ae2169e418f80f6505f3c24710582983405487f8
luizjuniorr <luiz.fernando.junior@eaportal.org>
2025-04-21 19:19:38 -0300

    Criação do componente para o card final e ajuste do transition dos botões

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
-import illustrationThankYou from "./images/illustration-thank-you.svg"
 import { CardInicial } from "./components/CardInicial"
+import { CardFinal } from "./components/CardFinal"
 
 export function App(){
   const [notaAvaliacao, setNotaAvaliacao] = useState(0)
@@ -13,15 +13,7 @@ export function App(){
       {submited === false ? (
         <CardInicial setNotaAvaliacao={setNotaAvaliacao} notaAvaliacao={notaAvaliacao} setSubmited={setSubmited} />
       ) : (
-        <div className="bg-gradient-dark mx-6 p-6 rounded-2xl text-white font-overpass text-center max-w-103">
-          <img className="mx-auto mb-6" src={illustrationThankYou} alt="" />
-          
-          <p className="mb-6 text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl">You selected {notaAvaliacao} out of 5!</p>
-
-          <h1 className="text-2xl font-bold mb-2.5">Thank You!</h1>
-    
-          <p className="text-light-gray text-sm leading-1">We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
-        </div>
+        <CardFinal notaAvaliacao={notaAvaliacao}/>
       )}
     </div>
   )
